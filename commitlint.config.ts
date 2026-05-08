805dbdb66ea9d2f8c10aaf6d92149544451e437a
LucaMezz <lucamezza4@gmail.com>
2026-05-08 20:11:46 +1000

    chore(repo): add cz-git commit prompt

---
+++ commitlint.config.ts
@@ -0,0 +1,11 @@
+// oxlint-disable-next-line import/no-default-export
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      ["api", "api-client", "core", "desktop", "web", "ui", "deps", "ci", "config", "repo"],
+    ],
+  },
+};
